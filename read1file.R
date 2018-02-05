c62450a9160b02c673a508d3b747e20bf1a59582
zapperosa <zapperosa@gmail.com>
2018-02-05 01:08:32 +0100

    final report - fixing output file

--- read1file.R
+++ read1file.R
@@ -3,13 +3,10 @@ install.packages("pheatmap")
 require(ggplot2) 
 require(reshape2)
 
+
 file1 <- file.choose()
 
 #Read first lines of file to acquire necessary information
-
-
-#Step 3: Data acquisition - Manual download of the full egg tar, using two diferent users, alcoholic and non-alcoholic
-# with 120 trials each
 
 setwd("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/assignmentArchives/co2a0000447")
 getwd()
@@ -82,16 +79,18 @@ userInfo[4] <- "SAMPLE"
 userInfo[5] <- "TRIAL"
 
 
-for(q in 1: 3){
+for(q in 1: 2){
   if (q==1) {
     setwd("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/assignmentArchives/co2a00004447")
     fileList <- list.files("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/assignmentArchives/co2a00004447")
     connFile <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/alcoholicTable.txt", 'a')  
+    connFileAll <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/bigTable.txt", 'a')
     } 
   else{ 
     setwd("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/assignmentArchives/co2c0000337")
     fileList <- list.files("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/assignmentArchives/co2c0000337")
     connFile <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/userTable.txt", 'a')  
+    connFileAll <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/bigTable.txt", 'a')
     }
     
     
@@ -113,6 +112,7 @@ for(q in 1: 3){
     header <- c(userInfo, sensors)
     header <- paste(unlist(header), collapse= " ")
     write(header, connFile, sep = " ")
+    write(header, connFileAll, sep = " ")
   }
 
   for(i in 1: 256){
@@ -130,28 +130,31 @@ for(q in 1: 3){
     }
   insertRow <- paste(unlist(sensorsRow), collapse=" ")
   write(insertRow, connFile, sep = " ")
+  write(insertRow, connFileAll, sep = " ")
   }
     }
+  close(connFileAll)
   close(connFile)
 }
-
+fileGeneral <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/bigTable.txt")
 fileA <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/alcoholicTable.txt")
 fileC <- file("C:/Users/Usuari/Documents/UNI/4/4_2/BigDataScience/assignment1/userTable.txt")
 dfAlc <- read.table(fileA, header = TRUE, sep = " ")
 dfCon <- read.table(fileC, header = TRUE, sep = " ")
+df <- read.table(fileGeneral, header = TRUE, sep = " ")
+df
 dfAlc
 dfCon
 
-p <- matrix()
-p <- df
-p
-
+head(df)
 #Step 4: Data exploration
+summary(dfCon)
+summary(dfAlc)
 summary(df)
+
 pheatmap(t(scale(as.matrix(df))), show_colnames=TRUE)
 
 
-head(df)
 #4.1. Exercise 1 - Represent the 'FP1' channel (first one).
 
 table(df$FP1)
@@ -159,25 +162,45 @@ table(df$FP1)
 title('EEG reading Uiii Repjjj Paradkkk (Alcoholic')
 xlabel('Time(sec)')
 ylabel('Voltage(mv)')
-ggplot(df, aes(x=SAMPLE, y=FP1)) + geom_point()
 
+meanCon <- mean(dfCon$FP1)
+medCon <- median(dfCon$FP1)
+
+meanAlc <- mean(dfAlc$F8)
+medAlc <- median(dfAlc$FP1)
+
+ggplot(dfCon, aes(x=SAMPLE, y=FP1)) + geom_point()  + facet_grid(. ~ EXP_TYPE)
+
+ggplot(dfAlc, aes(x=SAMPLE, y=FP1)) + geom_point() + facet_grid(. ~ EXP_TYPE)
 
 #4.2. Represent the 'FP1' channel as well as the next three
-
-ggplot(df, aes(x=SAMPLE, y=FP1)) +geom_point()
   
-ggplot(df, aes(x=SAMPLE, y=FP2))+geom_point() 
-ggplot(df, aes(x=SAMPLE, y=F7))+geom_point() 
-ggplot(df, aes(x=SAMPLE, y=F8))+geom_point()
+ggplot(dfCon, aes(x=SAMPLE, y=FP2))+geom_point()+ facet_grid(. ~ EXP_TYPE)
+ggplot(dfAlc, aes(x=SAMPLE, y=FP2))+geom_point()+ facet_grid(. ~ EXP_TYPE)
 
+ggplot(dfCon, aes(x=SAMPLE, y=F7))+geom_point() + facet_grid(. ~ EXP_TYPE)
+ggplot(dfAlc, aes(x=SAMPLE, y=F7))+geom_point()+ facet_grid(. ~ EXP_TYPE)
+
+ggplot(dfCon, aes(x=SAMPLE, y=F8))+geom_point() + facet_grid(. ~ EXP_TYPE)
+ggplot(dfAlc, aes(x=SAMPLE, y=F8))+geom_point() + facet_grid(. ~ EXP_TYPE)
 #4.3. Represent all 64 channels
 
 
+col <- table(dfAlc$FP1)
 
 
+#Step 5
+#Exercise: Clean the data set
+for(a in 6: 69){
+  meanCol <- mean(col)
+  dfTest <- dfAlc[a][1]
+  for(z in 1: length(col)){
+    col[z] <- col[z]-meanCol
+  }
+  dfAlc[a][1] <- col
+}
 
-
-
+#Exercise: Obtain the quantiles
 
 
 
